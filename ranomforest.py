805789736934a1dde695f82c20dad5e1d1b484f3
kawa <kawahara.aislab@gmail.com>
2024-11-17 21:16:21 +0900

    edit: randomforest

--- ranomforest.py
+++ ranomforest.py
@@ -9,19 +9,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
 
-data_path = "./dataset/" # 各画像ファイルが「no_weed」，「exist_weed」に分けとく
-image_size = (640, 480)
+data_path = "./datasets/train/" # 各画像ファイルが「no_weed」，「exist_weed」に分けとく
+val_path = "./datasets/val/" 
+image_size = (522, 318)
 
 # 画像とラベルの読み込み
 def load_images_and_labels(data_path):
     images = []
     labels = []
-    label_folders = {'no_weed': 0, 'exist_weed': 1}
+    label_folders = {'no_weed': 0, 'little_weed': 1, 'many_weed':2}
     for label_name, label in label_folders.items():
         folder_path = os.path.join(data_path, label_name)
         for filename in os.listdir(folder_path):
             img_path = os.path.join(folder_path, filename)
-            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            img = cv2.imread(img_path, cv2.COLOR_BGR2RGB)
             if img is not None:
                 img = cv2.resize(img, image_size)
                 images.append(img.flatten())  # 1次元ベクトルに変換
@@ -32,14 +33,15 @@ def load_images_and_labels(data_path):
 X, y = load_images_and_labels(data_path)
 
 # 訓練データとテストデータに分割
-train_images, test_images, train_labels, test_labels = train_test_split(X, y, test_size=0.2, random_state=42)
+train_images, train_labels  = load_images_and_labels(data_path)
+test_images, test_labels = load_images_and_labels(val_path)
 
 # ランダムフォレストのモデルを作成
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(train_images, train_labels)
 
 # 学習済みモデルを保存
-model_name = "random_forest_weed.joblib"
+model_name = "random_forest_weed_3classes.joblib"
 model_path = os.path.join("./models", model_name)
 joblib.dump(rf_model, model_path)
 print(f"Model saved to {model_path}")
